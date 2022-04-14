660991598d196d0eaa8353f942e577b3fa5501ff
hansonpohan <a0931771160@gmail.com>
2022-04-14 09:14:04 +0800

    no message

---
+++ pohan上課內容C#/C#語言/myC#/ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while 迴圈
+            while (true)
+            {
+                Console.WriteLine("Hello World");
+                break;
+            }
+            Console.WriteLine("---------------------------");
+            int d = 1;
+            while (true)
+            {
+                if (d>10)
+                {
+                    break;
+                }
+
+                Console.WriteLine("d:" + d);
+                d += 1;
+            }
+            Console.WriteLine("---------------------------");
+            int dd = 1;
+            while (dd <= 10)
+            {
+                Console.WriteLine("dd:" + dd);
+                dd += 1;
+            }
+            Console.WriteLine("---------------------------");
+
+            //do ... while 迴圈先執行後判斷至少會跑一次
+            do
+            {
+                Console.WriteLine("Hello World");
+            }
+            while (false);
+
+            int g = 1;
+            do
+            {
+                Console.WriteLine("g:" + g);
+                g += 1;
+            }
+            while (g <= 10);
+            Console.WriteLine("---------------------------");
+
+
+
+
+
+
+
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("按任意鍵結束程式");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ pohan上課內容C#/C#語言/老師教學C#語言/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Loop 迴圈示範
+            for (int i = 1; i <= 20; i += 1) //step 步進1
+            {
+                Console.WriteLine("i:" + i); 
+            }
+            Console.WriteLine("----------------------------");
+
+            for (int i = 2; i <= 100; i += 2) //偶數 2...100
+            {
+                Console.WriteLine("i:" + i);
+            }
+            Console.WriteLine("----------------------------");
+            //輸出 10...1
+            for (int i = 10; i >= 1; i -= 1)
+            {
+                Console.WriteLine("i:" + i);
+            }
+            Console.WriteLine("----------------------------");
+
+            for (int i = 1; i <= 20; i += 1)
+            {
+                if (i > 9)
+                {
+                    break; //中斷迴圈;
+                }
+                Console.WriteLine("i:" + i);
+            }
+            Console.WriteLine("----------------------------");
+
+            for (int i = 1; i <= 20; i += 1)
+            {
+                if (i == 8)
+                {
+                    continue; //跳過此次迴圈;
+                }
+                Console.WriteLine("i:" + i);
+            }
+            Console.WriteLine("----------------------------");
+
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("按任意鍵結束");
+            Console.ReadKey();
+        }
+    }
+}
